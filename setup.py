587beb553d535a89954c9ce97e045634a8300d3f
Takashi Ando <dodo5522@gmail.com>
2015-04-09 08:33:11 +0900

    Add setup.py and test.py but they are not implemented yet.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup, find_packages
+
+setup(
+    name="tsmppt60_driver",
+    version="0.0.1",
+    description="Python module to access TS-MPPT-60",
+    author="Takashi Ando",
+    url="https://github.com/dodo5522/tsmppt60_driver",
+    packages=find_packages())
