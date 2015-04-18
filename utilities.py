ee22e8d74c893360a90d861d3af502d978388fd4
Takashi Ando <dodo5522@gmail.com>
2015-04-18 19:10:27 +0900

    Integrate mb.py into utilities.py.

--- utilities.py
+++ utilities.py
@@ -6,11 +6,102 @@ TS-MPPT-60 data structure library.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-from .mb import ManagementBase
+import requests
+import logging
 
 
-class Data(object):
-    """ Super class to get data about charging status.
+class ManagementBase(object):
+    """ Class to get raw data from TS-MPPT-60.
+    """
+    _MBID = 0x01
+    _MBP = 502
+
+    _V_SCALE_LDEC = 0
+    _V_SCALE_RDEC = 1
+    _I_SCALE_LDEC = 2
+    _I_SCALE_RDEC = 3
+
+    def __init__(self, host, cgi="MBCSV.cgi", debug=False):
+        """
+        Keyword arguments:
+            host: Host address like "192.168.1.20" of TS-MPPT-60 live view
+            cgi: CGI file name to get the information
+            debug: If True, logging is enabled.
+        """
+        self._logger = logging.getLogger(type(self).__name__)
+        self._logger.addHandler(logging.StreamHandler())
+
+        if debug:
+            self._logger.setLevel(logging.DEBUG)
+
+        self._url = "http://" + host + "/" + cgi
+
+        def _get_scale(ldec, rdec):
+            L = self.read_modbus(self._MBID, ldec, 1)
+            R = self.read_modbus(self._MBID, rdec, 1)
+            return L + str(int(R) / 65535)
+
+        self.vscale = float(_get_scale(self._V_SCALE_LDEC, self._V_SCALE_RDEC))
+        self.iscale = float(_get_scale(self._I_SCALE_LDEC, self._I_SCALE_RDEC))
+
+    def _get(self, mbid, addr, reg, field=4):
+        """ Get raw data against MBID, Address, Register, and Field.
+
+        Keyword arguments:
+            mbid: MBID
+            addr: Address to get information
+            reg: Register to get information
+            field: Field to get information
+
+        Returns: String like "1,4,1,1,1"
+        """
+        params = []
+        params.append("ID=" + str(mbid))
+        params.append("F=" + str(field))
+        params.append("AHI=" + str(addr >> 8))
+        params.append("ALO=" + str(addr & 255))
+        params.append("RHI=" + str(reg >> 8))
+        params.append("RLO=" + str(reg & 255))
+
+        res = requests.get("{0}?{1}".format(self._url, "&".join(params)))
+        self._logger.debug("{0}".format(res.request.url))
+
+        return res.text
+
+    def read_modbus(self, mbid=_MBID, address=0, register=0):
+        """ Read the value against MBID, Address, and Register.
+
+        Keyword arguments:
+            mbid: MBID
+            addr: Address to get information
+            reg: Register to get information
+
+        Returns: String with short integer (ex. 16bit value).
+        """
+        raw_value_str = self._get(mbid, address, register)
+        raw_values = [int(v) for v in raw_value_str.split(",")]
+        idx_max = raw_values[2]
+        idx_value = 3
+        ret_str = ""
+
+        self._logger.debug(raw_value_str)
+
+        while idx_value < idx_max + 2:
+            ret_short = (raw_values[idx_value] * 256)
+            idx_value += 1
+            ret_short += raw_values[idx_value]
+            idx_value += 1
+
+            if idx_value < idx_max + 2:
+                ret_str += str(ret_short) + "#"
+            else:
+                ret_str += str(ret_short)
+
+        return ret_str
+
+
+class _ChargeControllerStatus(object):
+    """ Abstract class to get data about charge controller status.
     """
 
     def __init__(self, mb, group):
@@ -103,7 +194,7 @@ class Data(object):
         raise NotImplementedError
 
 
-class BatteryData(Data):
+class BatteryStatus(_ChargeControllerStatus):
     """ Class to get data about charging battery.
     """
 
@@ -112,7 +203,7 @@ class BatteryData(Data):
         Keyword arguments:
             mb: instance of ManagementBase class
         """
-        Data.__init__(self, mb, "Battery")
+        _ChargeControllerStatus.__init__(self, mb, "Battery")
 
     def get_params(self):
         """ Get a list of all params for battery charging status.
@@ -124,7 +215,7 @@ class BatteryData(Data):
             (58, "W", "Output Power", 1))
 
 
-class SolarArrayData(Data):
+class SolarArrayStatus(_ChargeControllerStatus):
     """ Class to get data about solar array.
     """
 
@@ -133,7 +224,7 @@ class SolarArrayData(Data):
         Keyword arguments:
             mb: instance of ManagementBase class
         """
-        Data.__init__(self, mb, "Array")
+        _ChargeControllerStatus.__init__(self, mb, "Array")
 
     def get_params(self):
         """ Get a list of all params for solar array status.
@@ -146,7 +237,7 @@ class SolarArrayData(Data):
             (60, "W", "Sweep Pmax", 1))
 
 
-class TemperaturesData(Data):
+class TemperaturesStatus(_ChargeControllerStatus):
     """ Class to get data about temperatures sensors.
     """
 
@@ -155,7 +246,7 @@ class TemperaturesData(Data):
         Keyword arguments:
             mb: instance of ManagementBase class
         """
-        Data.__init__(self, mb, "Temperatures")
+        _ChargeControllerStatus.__init__(self, mb, "Temperatures")
 
     def get_params(self):
         """ Get a list of all params for temperature sensors.
@@ -163,7 +254,7 @@ class TemperaturesData(Data):
         return ()
 
 
-class ResettableCountersData(Data):
+class CountersStatus(_ChargeControllerStatus):
     """ Class to get data about resettable counters.
     """
 
@@ -172,7 +263,7 @@ class ResettableCountersData(Data):
         Keyword arguments:
             mb: instance of ManagementBase class
         """
-        Data.__init__(self, mb, "Resettable Counters")
+        _ChargeControllerStatus.__init__(self, mb, "Resettable Counters")
 
     def get_params(self):
         """ Get a list of all params for resettable counters.
@@ -191,7 +282,7 @@ class LiveData(object):
 
         self._data_objects = {
             obj._group: obj for obj in (
-                BatteryData(self._mb),
-                SolarArrayData(self._mb),
-                TemperaturesData(self._mb),
-                ResettableCountersData(self._mb))}
+                BatteryStatus(self._mb),
+                SolarArrayStatus(self._mb),
+                TemperaturesStatus(self._mb),
+                CountersStatus(self._mb))}
