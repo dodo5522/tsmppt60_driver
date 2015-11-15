708fe83fc447a8dd31714d01a66d3e54c4ea652f
Takashi Ando <dodo5522@gmail.com>
2015-11-15 09:23:26 +0900

    Update data structure.

--- tsmppt60_driver/__init__.py
+++ tsmppt60_driver/__init__.py
@@ -17,42 +17,65 @@ class SystemStatus(object):
     """ This is iterator object containing the system status of TS-MPPT-60.
         Use this object like below.
 
-        status_all = StatusAll("192.168.1.20")
+        system_status= SystemStatus("192.168.1.20").get()
 
-        for stat in status_all:
-            # This stat has some group and you can get it by str().
-            group = str(stat)
-            # get_all_status method returns all data of the stat.
-            live_data = stat.get_status_all()
-            # live_data is tuple. first item is id like "Battery Voltage"
-            print("id: {}".format(live_data[0]))
-            # second item is value like "12.4"
-            print("val: {}".format(live_data[1]))
-            # third item is unit like "V"
-            print("unit: {}".format(live_data[2]))
+        # get() method returns dict and you can parse it like below.
+        for key, data in system_status.items():
+            # get element name. (like "Battery Voltage", ...)
+            elem = key
+            # get group. (like "Battery", "Array", ...)
+            group = data["group"]
+            # get value. (like 24.1[V])
+            value = data["value"]
+            # get unit. (like "kWh")
+            unit = data["unit"]
     """
 
     def __init__(self, host):
         _mb = ManagementBase(host)
 
-        self._status = (
+        self._devices = (
             BatteryStatus(_mb),
             SolarArrayStatus(_mb),
             TemperaturesStatus(_mb),
             CountersStatus(_mb))
 
+    def get(self):
+        """ Get all status of devices.
+
+        Returns:
+            {
+                "Battery Voltage":{
+                    "group": "Battery",
+                    "value": 12.1,
+                    "unit": "V"},
+                "Charge Current":{
+                    "group": "Battery",
+                    "value": 8.4,
+                    "unit": "A"}
+            }
+        """
+        status_dict = {}
+
+        for device in self._devices:
+            for status in device.get_status_all():
+                label = status.pop("label")
+                status_dict[label] = status
+
+        return status_dict
+
     def __len__(self):
-        return len(self._status)
+        return len(self._devices)
 
     def __iter__(self):
         self._index = 0
         return self
 
     def __next__(self):
-        if self._index >= len(self._status):
+        if self._index >= len(self._devices):
             raise StopIteration
 
-        stat_obj = self._status[self._index]
+        stat_obj = self._devices[self._index]
         self._index += 1
 
         return stat_obj
