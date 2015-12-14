c47cf5628aff2fda28caba7b66e74d0b9bd2991a
Takashi Ando <dodo5522@gmail.com>
2015-12-15 00:18:14 +0900

    Add author_email.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version="0.0.7",
     description="Python module to get data from TS-MPPT-60.",
     author="Takashi Ando",
+    author_email="takashi7ando@gmail.com",
     url="https://github.com/dodo5522/tsmppt60_driver",
     install_requires=[
         "requests>=2.6.0"],
