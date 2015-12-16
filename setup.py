71929c453856e149661ed6738483243217497c16
Takashi Ando <dodo5522@gmail.com>
2015-12-16 10:35:38 +0900

    Update readme document.

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import os
 from setuptools import setup
+
+
+def readme():
+    try:
+        os.system("pandoc --from=markdown --to=rst README.md -o README.rst")
+        with open("README.rst", "r") as f:
+            return f.read()
+    except:
+        return ""
 
 setup(
     name="tsmppt60-driver",
     version="0.0.7",
-    description="Python module to get data from TS-MPPT-60.",
+    description="To get status of your solar charge controller TS-MPPT-60.",
+    long_description=readme(),
+    license="GPLv2",
     author="Takashi Ando",
     author_email="takashi7ando@gmail.com",
     url="https://github.com/dodo5522/tsmppt60_driver",
