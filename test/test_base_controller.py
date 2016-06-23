612a9123348f8b49129fdfeb3446deb7f62b106a
Takashi Ando <dodo5522@gmail.com>
2016-06-24 01:05:46 +0900

    To clear the test scenario for #6.

--- test/test_base_controller.py
+++ test/test_base_controller.py
@@ -6,7 +6,6 @@ try:
     from unittest.mock import patch
 except:
     from mock import patch
-from tsmppt60_driver.base import ModbusRegisterTable
 from tsmppt60_driver.base import ManagementBase
 from tsmppt60_driver.status import BatteryStatus
 from tsmppt60_driver.status import SolarArrayStatus
@@ -33,31 +32,26 @@ class TestChargeControllerStatus(unittest.TestCase):
     """ Test case for ChargeControllerStatus. """
 
     @classmethod
-    def _requests_get(cls, url, timeout):
-
-        def _url_parm(modbus_registers):
-            addr_hi = str(int(modbus_registers[0]) >> 8)
-            addr_lo = str(int(modbus_registers[0]) & 255)
-            reg_hi = str(int(modbus_registers[-1]) >> 8)
-            reg_lo = str(int(modbus_registers[-1]) & 255)
-
-            return 'ID=1&F=4&AHI={}&ALO={}&RHI={}&RLO={}' \
-                .format(addr_hi, addr_lo, reg_hi, reg_lo)
-
-        table_scaling = {
-            _url_parm(ModbusRegisterTable.VOLTAGE_SCALING_HIGH): "1,4,2,0,180",
-            _url_parm(ModbusRegisterTable.VOLTAGE_SCALING_LOW): "1,4,2,0,0",
-            _url_parm(ModbusRegisterTable.CURRENT_SCALING_HIGH): "1,4,2,0,80",
-            _url_parm(ModbusRegisterTable.CURRENT_SCALING_LOW): "1,4,2,0,0",
-            _url_parm(ModbusRegisterTable.BATTERY_VOLTAGE): "1,4,2,17,160"}  # 24.78515625
-
-        text = table_scaling[str(url).split("?")[-1]]
-        return DummyResponse(url, text)
+    def _gen_url_parm(cls, addr, reg):
+        return 'ID=1&F=4&AHI={}&ALO={}&RHI={}&RLO={}' \
+            .format(addr >> 8, addr & 255, reg >> 8, reg & 255)
 
     @classmethod
     @patch("tsmppt60_driver.base.requests.get", auto_spec=True)
     def setUpClass(cls, patched_get):
-        patched_get.side_effect = cls._requests_get
+        def _requests_get(url, timeout):
+            mb_url_parm = str(url).split("?")[-1]
+
+            table_scaling = {
+                cls._gen_url_parm(0x0000, 1): "1,4,2,0,180",  # VOLTAGE_SCALING_HIGH
+                cls._gen_url_parm(0x0001, 1): "1,4,2,0,0",    # VOLTAGE_SCALING_LOW
+                cls._gen_url_parm(0x0002, 1): "1,4,2,0,80",   # CURRENT_SCALING_HIGH
+                cls._gen_url_parm(0x0003, 1): "1,4,2,0,0"}    # CURRENT_SCALING_LOW
+
+            text = table_scaling[mb_url_parm]
+            return DummyResponse(url, text)
+
+        patched_get.side_effect = _requests_get
 
         mb = ManagementBase("dummy.uribou.mydns.jp")
         cls._bat = BatteryStatus(mb)
@@ -77,15 +71,24 @@ class TestChargeControllerStatus(unittest.TestCase):
 
     @patch("tsmppt60_driver.base.requests.get", auto_spec=True)
     def test_get_battery_voltage(self, patched_get):
+        def _requests_get(url, timeout):
+            mb_url_parm = str(url).split("?")[-1]
+
+            table_scaling = {
+                self._gen_url_parm(0x0026, 1): "1,4,2,17,160"}  # BATTERY_VOLTAGE, 24.78515625
+
+            text = table_scaling[mb_url_parm]
+            return DummyResponse(url, text)
+
+        patched_get.side_effect = _requests_get
+
         expected_value = {
             "group": "Battery",
             "label": "Battery Voltage",
             "value": 24.78515625,
             "unit": "V"}
 
-        patched_get.side_effect = self._requests_get
-
-        value = self._bat.get_status(38, 'V', 'Battery Voltage', 1)
+        value = self._bat.get_status(0x0026, 'V', 'Battery Voltage', 1)
 
         self.assertEqual(set(expected_value.items()), set(value.items()))
 
